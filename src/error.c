e17a685f49e8a6af7eeaa3d41fc998efc729d717
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-17 11:21:51 +0530

    implement error_from

--- src/error.c
+++ src/error.c
@@ -51,6 +51,38 @@ Error *error_propagate(Error *cause, Location loc)
     return err;
 }
 
+Error *error_from(Error *cause, const Domain *domain, uint32_t code, Location loc, const char *fmt, ...)
+{
+    Error *err = malloc(sizeof(Error));
+    if (!err)
+        return NULL;
+
+    va_list args, args_copy;
+    va_start(args, fmt);
+    va_copy(args_copy, args);
+
+    int len = vsnprintf(NULL, 0, fmt, args);
+    va_end(args);
+
+    err->message = malloc(len + 1);
+    if (!err->message)
+    {
+        va_end(args_copy);
+        free(err);
+        return NULL;
+    }
+
+    vsnprintf(err->message, len + 1, fmt, args_copy);
+    va_end(args_copy);
+
+    err->domain = domain;
+    err->code = code;
+    err->location = loc;
+    err->cause = cause;
+
+    return err;
+}
+
 void error_free(Error **err)
 {
     if (!err || !*err)
